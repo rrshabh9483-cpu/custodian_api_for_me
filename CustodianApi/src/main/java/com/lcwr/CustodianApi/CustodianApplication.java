9f1d0b301c898fba8bacbe322ed56d91d053f2a4
rrshabh9483-cpu <rrshabh9483@gmail.com>
2025-08-29 10:37:39 +0530

    okiiiiiiiiii

--- CustodianApi/src/main/java/com/lcwr/CustodianApi/CustodianApplication.java
+++ CustodianApi/src/main/java/com/lcwr/CustodianApi/CustodianApplication.java
@@ -8,6 +8,8 @@ public class CustodianApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CustodianApplication.class, args);
+
+		System.out.println("Good Morning!");
 	}
 
 }
